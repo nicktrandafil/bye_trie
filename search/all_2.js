7d54562c59657c1ab30a5442ab7ef68bc0a18cda
nicktrandafil <nicktrandafil@users.noreply.github.com>
2024-12-17 23:13:34 +0000

    deploy: 62711a3d7fe4e4cf4330a9dd4a009cdc301e0824

--- search/all_2.js
+++ search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['erase_0',['erase',['../classbye__trie_1_1ByeTrie.html#a214fea35a148229e58c57eeac2872002',1,'bye_trie::ByeTrie::erase(Bits&lt; P &gt; prefix) noexcept(noexcept(alloc_.realloc(MemBlk{}, 0)))'],['../classbye__trie_1_1ByeTrie.html#adae9bd70f66630d260da55782a460f65',1,'bye_trie::ByeTrie::erase(ByeTrieIterator&lt; P, T, N &gt; it) noexcept(false)']]]
+  ['erase_5fexact_0',['erase_exact',['../classbye__trie_1_1ByeTrie.html#acf996e6db82aaf1b09ce4508341ea531',1,'bye_trie::ByeTrie']]]
 ];
